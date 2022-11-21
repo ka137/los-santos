04b909b05c9aecadd210f955d0478eda1a651bd5
Karen <113837146+ka137@users.noreply.github.com>
2022-11-21 07:41:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('1')
+let currentDate = new Date();
+let day = currentDate.getDay()
+let id = Array.from(document.querySelector('#weekdays').children)
+if( day === 0){
+    id[6].classList.add('current')
+}
+else{
+    id[day - 1].classList.add('current')
+}
